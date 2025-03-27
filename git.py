312efed1a1e4b6e30e80a36694ce33f2b5ccabc0
Dibyanshu <purimagogoi789@gmail.com>
2025-03-27 21:22:02 +0530

    random password generater is added

--- git.py
+++ git.py
@@ -8,3 +8,7 @@ print(d)
 
 e=(input("enter name"))
 print(e)
+
+import random, string
+password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+print(password)  # Example: "X7fB2pLz"
